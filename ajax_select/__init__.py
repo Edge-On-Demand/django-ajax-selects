566867a2b5c6a08fbd7a4c2f90753fe19ad00064
chrisspen <chrisspen@gmail.com>
2024-05-23 13:20:21 -0400

    Refactored packaging.

--- ajax_select/__init__.py
+++ ajax_select/__init__.py
@@ -9,14 +9,16 @@ __author__ = "crucialfelix"
 __contact__ = "crucialfelix@gmail.com"
 __homepage__ = "https://github.com/Edge-On-Demand/django-ajax-selects/"
 
+# These won't be avaiable yet when accessed by setup.py.
 try:
     from django.apps import AppConfig  # noqa
+
+    from ajax_select.registry import registry, register  # noqa
+    from ajax_select.helpers import make_ajax_form, make_ajax_field  # noqa
+    from ajax_select.lookup_channel import LookupChannel  # noqa
+
 except (ImportError, ModuleNotFoundError):
     pass
-
-from ajax_select.registry import registry, register  # noqa
-from ajax_select.helpers import make_ajax_form, make_ajax_field  # noqa
-from ajax_select.lookup_channel import LookupChannel  # noqa
 
 # django 1.7+ will use the new AppConfig api
 # It will load all your lookups.py modules
